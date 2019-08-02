55305dde775337bac949213154dda1a88bae93f6
KavindaHarshana <51896118+KavindaHarshana@users.noreply.github.com>
2019-08-02 16:05:45 +0530

    Update GroupedCmsMenu.php

--- src/Extensions/GroupedCmsMenu.php
+++ src/Extensions/GroupedCmsMenu.php
@@ -1,107 +1,177 @@
 <?php
+
+namespace Symbiote\GroupedCmsMenu\Admin;
+
+use SilverStripe\Admin\LeftAndMain;
+use SilverStripe\Admin\LeftAndMainExtension;
+use SilverStripe\View\Requirements;
+use SilverStripe\ORM\ArrayList;
+use SilverStripe\ORM\GroupedList;
+use SilverStripe\View\ArrayData;
+use SilverStripe\ORM\FieldType\DBField;
+use SilverStripe\ORM\FieldType\DBText;
+use SilverStripe\ORM\SS_List;
+
 /**
- * Decorates left and main to provide a grouped/nested CMS menu.
+ * Decorates {@link LeftAndMain} to provide a grouped/nested CMS menu.
  *
  * @package grouped-cms-menu
  */
+class GroupedCmsMenu extends LeftAndMainExtension
+{
+    /**
+     * @var array
+     */
+    private static $menu_groups = [];
+    /**
+     * When you have larger menus, and/or multiple modules combining to the same menu,
+     * this may require something more consistent.
+     *
+     * @var boolean
+     */
+    private static $menu_groups_alphabetical_sorting = false;
 
-namespace Symbiote\GroupedCMSMenu\Extensions;
+    /**
+     * Require the CSS which we need for the menu.
+     *
+     * @return void
+     */
+    public function init()
+    {
+        parent::init();
+        Requirements::css('symbiote/silverstripe-grouped-cms-menu:client/dist/css/GroupedCmsMenu.css');
+    }
 
-use SilverStripe\Admin\CMSMenu;
-use SilverStripe\Admin\LeftAndMain;
-use SilverStripe\Core\Config\Config;
-use SilverStripe\Core\Extension;
-use SilverStripe\ORM\ArrayList;
-use SilverStripe\ORM\FieldType\DBText;
-use SilverStripe\ORM\GroupedList;
-use SilverStripe\View\ArrayData;
+    /**
+     * @return ArrayList
+     */
+    public function GroupedMainMenu()
+    {
+        $items = $this->getOwner()->MainMenu();
+        $result = ArrayList::create();
+        $config = $this->getOwner()->config();
+        $groupSettings = $config->get('menu_groups');
+        $itemsToGroup = [];
+        $groupSort = 0;
+        $itemSort = 0;
+        foreach ($groupSettings as $groupName => $menuItems) {
+            if (!count($menuItems['children'])) {
+                continue;
+            }
+            foreach ($menuItems['children'] as $key => $menuItem) {
+                $itemsToGroup[$menuItem] = [
+                    'Group' => $groupName,
+                    'Priority' => (array_key_exists('priority', $groupSettings[$groupName])) ? $groupSettings[$groupName]['priority'] : $groupSort,
+                    'SortOrder' => $itemSort
+                ];
+                $itemSort++;
+            }
+            $groupSort--;
+        }
+        foreach ($items as $item) {
+            $code = $item->Code;
+            if (array_key_exists($code, $itemsToGroup)) {
+                $item->Group = $itemsToGroup[$code]['Group'];
+                $item->Priority = $itemsToGroup[$code]['Priority'];
+                $item->SortOrder = $itemsToGroup[$code]['SortOrder'];
+            } else {
+                $item->Group = $code;
+                $item->Priority = is_numeric($item->MenuItem->priority) ? $item->MenuItem->priority : -1;
+                $item->SortOrder = 0;
+            }
+        }
+        $groupedList = GroupedList::create($items->sort(['Priority' => 'DESC']))->groupBy('Group');
+        foreach ($groupedList as $group => $children) {
+            if (count($children)) {
+                $active = false;
+                foreach ($children as $child) {
+                    if ($child->LinkingMode == 'current') {
+                        $active = true;
+                    }
+                }
+                $code = str_replace(' ', '_', $group);
+                $result->push(ArrayData::create([
+                    'Title' => $this->getTitle($group, $code),
+                    'IconClass' => $this->getIcon($group, $code),
+                    'Code' => DBField::create_field(DBText::class, $code),
+                    'Link' => $children->first()->Link,
+                    'LinkingMode' => $active ? 'current' : 'link',
+                    'Children' => $this->FilterChildren($children),
+                ]));
+            } else {
+                $result->push($children->first());
+            }
+        }
+        return $result;
+    }
 
-class GroupedCmsMenu extends Extension {
+    /**
+     * Return the {@link LeftAndMain} subclass' `$menu_title` for each Level 1
+     * menu item, or use the partial name of each child item from `$menu_groups`
+     * config.
+     *
+     * @param  string $group
+     * @param  string $code
+     * @return string
+     */
+    public function getTitle($group, $code)
+    {
+        $class = str_replace('-', '\\', $code);
+        if (class_exists($class)) {
+            return $class::create()->config()->get('menu_title');
+        }
+        return _t('GroupedCmsMenuLabel.' . $code, $group);
+    }
 
-	/**
-	 * @var array
-	 */
-	private static $menu_groups = array();
+    /**
+     * Return the {@link LeftAndMain} subclass' `$menu_icon_class` or use the
+     * name of the heading as per the `$menu_groups` setting in config.
+     *
+     * @param  string $group
+     * @param  string $code
+     * @return string
+     */
+    public function getIcon($group, $code)
+    {
+        $class = str_replace('-', '\\', $code);
+        $groupSettings = $this->getOwner()->config()->get('menu_groups');
+        if (class_exists($class)) {
+            return $class::create()->config()->get('menu_icon_class');
+        }
+        return 'font-icon-' . (!empty($groupSettings[$group]['icon']) ? $groupSettings[$group]['icon'] : '');
+    }
 
-	/**
-	 *	When you have larger menus, and/or multiple modules combining to the same menu, this may require something more consistent.
-	 */
-
-	private static $menu_groups_alphabetical_sorting = false;
-
-	/**
-	 * Require in CSS which we need for the menu
-	 */
-	public function init() {
-		// Requirements::css('grouped-cms-menu/css/GroupedCmsMenu.css');
-	}
-
-	/**
-	 * @return ArrayList
-	 */
-	public function GroupedMainMenu() {
-		$items  = $this->owner->MainMenu();
-		$result = ArrayList::create();
-		$config = Config::inst();
-		$groupSettings = $config->get(LeftAndMain::class, 'menu_groups');
-		$itemsToGroup = array();
-		$groupSort = 0;
-		$itemSort = 0;
-
-
-		foreach ($groupSettings as $groupName => $menuItems) {
-			if (count($menuItems['items'])) {
-				foreach ($menuItems['items'] as $key => $menuItem ) {
-					$itemsToGroup[CMSMenu::get_menu_code($menuItem)] = array(
-						'Group' => $groupName,
-						'Priority' => (array_key_exists('priority', $groupSettings[$groupName])) ? $groupSettings[$groupName]['priority'] : $groupSort,
-						'SortOrder' => $itemSort
-					);
-					$itemSort++;
-				}
-				$groupSort--;
-			}
-		}
-
-
-
-		foreach ($items as $item) {
-			$code = $item->Code;
-			if (array_key_exists($code, $itemsToGroup)) {
-				$item->Group = $itemsToGroup[$code]['Group'];
-				$item->Priority = $itemsToGroup[$code]['Priority'];
-				$item->SortOrder = $itemsToGroup[$code]['SortOrder'];
-			} else {
-				$item->Group = $code;
-				$item->Priority = is_numeric($item->MenuItem->priority) ? $item->MenuItem->priority : -1;
-				$item->SortOrder = 0;
-			}
-		}
-
-		foreach (GroupedList::create($items->sort(array('Priority'=>'DESC')))->groupBy('Group') as $group => $children) {
-			if (count($children) > 1) {
-				$active = false;
-				foreach ($children as $child) {
-					if ($child->LinkingMode == 'current') $active = true;
-				}
-				$icon = array_key_exists('icon', $groupSettings[$group]) ? $groupSettings[$group]['icon'] : false;
-                $iconClass = array_key_exists('icon_class', $groupSettings[$group]) ? $groupSettings[$group]['icon_class'] : false;
-				$code = str_replace(' ', '_', $group);
-				$result->push(ArrayData::create(array(
-					'Title' => _t('GroupedCmsMenuLabel.'.$code, $group),
-					'Code' => DBText::create_field('Text', $code),
-					'Link' => $children->First()->Link,
-					'Icon' => $icon,
-					'IconClass' => $iconClass,
-					'LinkingMode' => $active ? 'current' : '',
-					'Children' => $config->get('LeftAndMain', 'menu_groups_alphabetical_sorting') ? $children->sort('Title') : $children->sort('SortOrder')
-				)));
-			} else {
-				$result->push($children->First());
-			}
-		}
-
-		return $result;
-	}
-
+    /**
+     * Ensure what we pass to $Children in the include template is accurate. We only
+     * want to indicate to it that children should be shown groups declared in config.
+     *
+     * @param  SS_List $children
+     * @return ArrayList
+     */
+    public function filterChildren(SS_List $children)
+    {
+        // Only deal with children if we've explicitly instructed our classes as such
+        $config = $this->getOwner()->config();
+        $groupSettings = $config->get('menu_groups');
+        $filtered = ArrayList::create();
+        foreach ($children as $child) {
+            foreach ($groupSettings as $group => $candidates) {
+                $candidates = str_replace('-', '\\', $candidates['children']);
+                $class = $child->MenuItem->controller;
+                if (in_array($class, $candidates)) {
+                    foreach ($candidates as $candidate) {
+                        $menuItem = $candidate::create();
+                        $menuItem->setField('ChildTitle', $this->getTitle($group, $candidate));
+                        $menuItem->setField('Code', str_replace('\\', '-', $candidate));
+                        $menuItem->setField('IconClass', $this->getIcon($group, $candidate));
+                        $menuItem->setField('LinkingMode', $child->LinkingMode);
+                        $filtered->push($menuItem);
+                    }
+                }
+            }
+        }
+        return $config->get('menu_groups_alphabetical_sorting') ?
+            $filtered->sort('ChildTitle') :
+            $filtered->sort('SortOrder');
+    }
 }
