cec4d3126ec92d84397a04170a1b9043409cd63c
Sander Hagenaars <sander.ha@hotmail.com>
2020-06-01 08:24:56 +0200

    Fix menutitle and icon (#39)
    * Use correct method to return i18n menu title for grouped model admins
    
    * made custom icon files work for menu items. Added seperate template file for this

--- src/Admin/GroupedCmsMenu.php
+++ src/Admin/GroupedCmsMenu.php
@@ -2,7 +2,9 @@
 
 namespace Symbiote\GroupedCmsMenu\Admin;
 
+use SilverStripe\Admin\LeftAndMain;
 use SilverStripe\Admin\LeftAndMainExtension;
+use SilverStripe\Core\Convert;
 use SilverStripe\View\Requirements;
 use SilverStripe\ORM\ArrayList;
 use SilverStripe\ORM\GroupedList;
@@ -64,8 +66,8 @@ class GroupedCmsMenu extends LeftAndMainExtension
 
             foreach ($menuItems['children'] as $key => $menuItem) {
                 $itemsToGroup[$menuItem] = [
-                    'Group' => $groupName,
-                    'Priority' => (array_key_exists('priority', $groupSettings[$groupName])) ? $groupSettings[$groupName]['priority'] : $groupSort,
+                    'Group'     => $groupName,
+                    'Priority'  => (array_key_exists('priority', $groupSettings[$groupName])) ? $groupSettings[$groupName]['priority'] : $groupSort,
                     'SortOrder' => $itemSort
                 ];
                 $itemSort++;
@@ -89,7 +91,7 @@ class GroupedCmsMenu extends LeftAndMainExtension
         }
 
         $groupedList = GroupedList::create($items->sort(['Priority' => 'DESC']))->groupBy('Group');
-
+        $menuIconStyling = '';
         foreach ($groupedList as $group => $children) {
             if (count($children)) {
                 $active = false;
@@ -101,17 +103,32 @@ class GroupedCmsMenu extends LeftAndMainExtension
                 }
 
                 $code = str_replace(' ', '_', $group);
+                $class = str_replace('-', '\\', $code);
+                $iconClass = null;
+                $menuIcon = LeftAndMain::menu_icon_for_class($class);
+
+                if (!empty($menuIcon)) {
+                    $menuIconStyling .= $menuIcon;
+                } else {
+                    $iconClass = $this->getIcon($group, $code);
+                }
+
                 $result->push(ArrayData::create([
-                    'Title' => $this->getTitle($group, $code),
-                    'IconClass' => $this->getIcon($group, $code),
-                    'Code' => DBField::create_field(DBText::class, $code),
-                    'Link' => $children->first()->Link,
+                    'Title'       => $this->getTitle($group, $code),
+                    'IconClass'   => $iconClass,
+                    'HasCSSIcon'  => strtolower($code),
+                    'Code'        => DBField::create_field(DBText::class, $code),
+                    'Link'        => $children->first()->Link,
                     'LinkingMode' => $active ? 'current' : 'link',
-                    'Children' => $this->filterChildren($children),
+                    'Children'    => $this->filterChildren($children),
                 ]));
             } else {
                 $result->push($children->first());
             }
+        }
+
+        if ($menuIconStyling) {
+            Requirements::customCSS($menuIconStyling);
         }
 
         return $result;
@@ -122,8 +139,8 @@ class GroupedCmsMenu extends LeftAndMainExtension
      * menu item, or use the partial name of each child item from `$menu_groups`
      * config.
      *
-     * @param  string $group
-     * @param  string $code
+     * @param string $group
+     * @param string $code
      * @return string
      */
     public function getTitle($group, $code)
@@ -131,7 +148,7 @@ class GroupedCmsMenu extends LeftAndMainExtension
         $class = str_replace('-', '\\', $code);
 
         if (class_exists($class)) {
-            return $class::create()->config()->get('menu_title');
+            return LeftAndMain::menu_title($class);
         }
 
         return _t('GroupedCmsMenuLabel.' . $code, $group);
@@ -141,8 +158,8 @@ class GroupedCmsMenu extends LeftAndMainExtension
      * Return the {@link LeftAndMain} subclass' `$menu_icon_class` or use the
      * name of the heading as per the `$menu_groups` setting in config.
      *
-     * @param  string $group
-     * @param  string $code
+     * @param string $group
+     * @param string $code
      * @return string
      */
     public function getIcon($group, $code)
@@ -161,7 +178,7 @@ class GroupedCmsMenu extends LeftAndMainExtension
      * Ensure what we pass to $Children in the include template is accurate. We only
      * want to indicate to it that children should be shown groups declared in config.
      *
-     * @param  SS_List   $children
+     * @param SS_List $children
      * @return ArrayList
      */
     public function filterChildren(SS_List $children)
@@ -170,26 +187,39 @@ class GroupedCmsMenu extends LeftAndMainExtension
         $config = $this->getOwner()->config();
         $groupSettings = $config->get('menu_groups');
         $filtered = ArrayList::create();
-
+        $menuIconStyling = '';
         foreach ($children as $child) {
             foreach ($groupSettings as $group => $candidates) {
                 $candidates = str_replace('-', '\\', $candidates['children']);
                 $class = $child->MenuItem->controller;
+                $iconClass = null;
+                $menuIcon = LeftAndMain::menu_icon_for_class($class);
+
+                if (!empty($menuIcon)) {
+                    $menuIconStyling .= $menuIcon;
+                } else {
+                    $iconClass = $this->getIcon($group, $class);
+                }
 
                 if (in_array($class, $candidates)) {
                     $menuItem = $class::create();
                     $menuItem->setField('ChildTitle', $this->getTitle($group, $class));
                     $menuItem->setField('Code', str_replace('\\', '-', $class));
-                    $menuItem->setField('IconClass', $this->getIcon($group, $class));
+                    $menuItem->setField('IconClass', $iconClass);
+                    $menuItem->setField('HasCSSIcon', strtolower(Convert::raw2htmlname(str_replace('\\', '-', $class))));
                     $menuItem->setField('LinkingMode', $child->LinkingMode);
                     $filtered->push($menuItem);
                 }
             }
         }
 
+        if ($menuIconStyling) {
+            Requirements::customCSS($menuIconStyling);
+        }
+
         return $config->get('menu_groups_alphabetical_sorting') ?
-                $filtered->sort('ChildTitle') :
-                $filtered->sort('SortOrder');
+            $filtered->sort('ChildTitle') :
+            $filtered->sort('SortOrder');
     }
 
 }
